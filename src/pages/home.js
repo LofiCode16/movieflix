df58ebd4690f7d49b4c163df2f2cffaf718518ba
diegoski007 <diegopr178@gmail.com>
2019-12-05 13:11:33 -0300

    add popular and top movies in home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import useFetch from '../hooks/useFetch';
 import SliderMovies from '../components/SliderMovies';
-import { URL_API, API } from "../utils/constants"
+import { URL_API, API } from "../utils/constants";
+import { Row, Col } from 'antd';
+import MovieList from '../components/MovieList';
+
 
 export default function Home (){  
   const newMovies = useFetch(
     `${URL_API}/movie/now_playing?api_key=${API}&language=es-ES&page=1`
     );
 
-    console.log(newMovies);
+  const popularMovies = useFetch(
+    `${URL_API}/movie/popular?api_key=${API}&language=es-ES&page=1`
+    );
+   
+  const topRatedMovies = useFetch(
+    `${URL_API}/movie/top_rated?api_key=${API}&language=es-ES&page=1`
+    );  
     
   return(
     <>
       <SliderMovies movies={newMovies}/>
+      <Row>
+        <Col span={12}>
+          <MovieList title="Películas populares" movies={popularMovies} />
+        </Col>
+        <Col span={12}>
+          <MovieList title="Películas mejor valoradas" movies={topRatedMovies} />
+        </Col>
+      </Row>
     </> 
     );
 };
