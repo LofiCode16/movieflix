8954e6f85ca77a15b0f560a4933e25f038ec78a8
diegoski007 <diegopr178@gmail.com>
2019-12-12 11:23:13 -0300

    fetching last movies

--- src/pages/new-movies.js
+++ src/pages/new-movies.js
@@ -1,5 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Row, Col } from 'antd';
+import { URL_API, API } from '../utils/constants';
+import Footer from '../components/Footer';
+import Loading from '../components/Loading';
 
 export default function NewMovie(){
-  return <h2>Estamos en new movies</h2>
+  const [movieList, setMovieList] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`${URL_API}/movie/now_playing?api_key=${API}&language=es-ES&page=${page}`);
+      const movies = await response.json();
+      setMovieList(movies);
+    })();
+  }, [page]);
+
+  return(
+    <Row>
+      <Col span={24} style={{textAlign: 'center', marginTop: 25}}>
+        <h1 style={{fontSize: 35, fontWeight: 'bold'}}>
+          Ultimos lanzamientos
+        </h1>
+      </Col>
+      
+      {movieList.results ? (
+        <Col span={24}>        
+          <h2>Todas las peliculas</h2>        
+        </Col>
+      ) : (
+        <Col span={24}>        
+          <Loading />        
+        </Col>
+      )};
+      
+      <Col span={24}>
+        <Footer />
+      </Col>
+    </Row>
+  )
 }
