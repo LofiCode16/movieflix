c67c6c0af23b8ecd158c4da1da27098a567d1127
diegoski007 <diegopr178@gmail.com>
2020-01-11 18:45:23 -0300

    add modal for trailers

--- src/pages/movie/movie.js
+++ src/pages/movie/movie.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col, Button } from 'antd';
 import { useParams } from 'react-router-dom';
 import moment from 'moment';
 import useFetch from '../../hooks/useFetch';
 import { URL_API, API } from '../../utils/constants';
 import Loading from '../../components/Loading';
+import ModalVideo from '../../components/ModalVideo';
 
 import './movie.scss';
 
@@ -51,7 +52,43 @@ function PosterMovie(props){
 }
 
 function MovieInfo(props){
-  const {movieInfo: {id, title, release_date, overview, genres}} = props;
+  const { 
+    movieInfo: {id, title, release_date, overview, genres}
+  } = props;
+
+  const [isVisibleModal, setIsVisibleModal] = useState(false);
+  const videoMovie = useFetch(
+    `${URL_API}/movie/${id}/videos?api_key=${API}&language=es-ES`
+  );
+
+  const openModal = () => {
+    setIsVisibleModal(true);
+  }
+
+  const closeModal = () => {
+    setIsVisibleModal(false);
+  }
+
+  const renderVideo = () => {
+    if (videoMovie.result){
+      if (videoMovie.result.results.length > 0){
+        return (
+          <>
+            <Button icon="play-circle" onClick={openModal}>
+              Ver trailer
+            </Button>
+            <ModalVideo 
+              videoKey={videoMovie.result.results[0].key} 
+              videoPlatform={videoMovie.result.results[0].site}
+              isOpen={isVisibleModal}
+              close={closeModal}
+            />
+          </>  
+        )
+      }
+      
+    }
+  }
 
   return(
     <>
@@ -61,7 +98,7 @@ function MovieInfo(props){
             {moment(release_date, "YYYY-MM-DD").format("YYYY")}
           </span>
         </h1>
-        <button>Ver trailer</button>
+        {renderVideo()}
       </div>
       <div className="movie__info-content">
         <h3>General</h3>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 export const URL_API = "https://api.themoviedb.org/3";
 export const API = "6f02defc2d95c4145f46bc63743befb5";
+
